0d7cdd6d9570e45c6d9d8c79e224e11d21f7ecff
Abdalla Sarhan <abdalla1@my.yorku.ca>
2025-04-03 14:38:31 -0400

    refactor: change 'output' folder to 'input' for improved readability in deployment process

--- deployx-upload-service/src/index.ts
+++ deployx-upload-service/src/index.ts
@@ -16,8 +16,8 @@ app.use(express.json());
 app.post("/deploy", async (req, res) => {
     const repoUrl = req.body.repoUrl; // github.com/AbdallaSarhan/repo
     const id = generateId();
-    await simpleGit().clone(repoUrl, path.join(__dirname, `./output/${id}`));
-    const files = getAllFiles(path.join(__dirname, `./output/${id}`));
+    await simpleGit().clone(repoUrl, path.join(__dirname, `./input/${id}`));
+    const files = getAllFiles(path.join(__dirname, `./input/${id}`));
     // Upload every file to s3
     files.forEach(async (file) => {
         await uploadFileToS3(file.slice(__dirname.length + 1), file);
